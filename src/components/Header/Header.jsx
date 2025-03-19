4928f34cd5ce4ab52930671a416b185627ea2a24
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-03-19 14:23:34 -0400

    Add opening and closing modal function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,18 @@ import "./Header.css";
 import logo from "../../assets/WTWRLogo.svg";
 import avatar from "../../assets/WTWRAvatar.svg";
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} />
       <p className="header__date">DATE, LOCATION</p>
-      <button className="header__add-clothes-btn">ADD CLOTHES</button>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        ADD CLOTHES
+      </button>
       <div className="header__user">
         <p className="header__username">NAME</p>
         <img src={avatar} alt="NAME" className="header__avatar" />
