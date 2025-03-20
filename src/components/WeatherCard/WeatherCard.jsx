958841578967a595d628bc9a857e14f095193028
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-03-20 15:02:18 -0400

    Weather condition undefined

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,26 @@
 import "./WeatherCard.css";
-import cloudynight from "../../assets/CloudyNightBanner.png";
+import { weatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condtion
+    );
+  });
+
+  const weatherOption = filteredOptions[0];
+
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
-      <img src={cloudynight} alt="WEATHER" className="weather-card__img" />
+      <img
+        src={weatherOption?.url}
+        alt={`${weatherOption?.day ? "day" : "night"}, ${
+          weatherOption?.condition
+        }`}
+        className="weather-card__img"
+      />
     </section>
   );
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,66 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("..assets/day/dayclear.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("..assets/day/daycloudy.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("..assets/day/dayfog.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("..assets/day/dayrain.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "storm",
+    url: new URL("..assets/day/daystorm.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("..assets/day/daysnow.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("..assets/night/nightclear.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("..assets/night/nightcloudy.png", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "fog",
+    url: new URL("..assets/night/nightfog.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("..assets/night/nightrain.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "storm",
+    url: new URL("..assets/night/nightstorm.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("..assets/night/nightsnow.svg", import.meta.url).href,
+  },
+];
+
 export const defaultClothingItems = [
   {
     _id: 0,
