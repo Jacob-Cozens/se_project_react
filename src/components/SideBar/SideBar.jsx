abe713101f8ae76bc29c5f9a855e409c38abc00a
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-05-07 18:03:45 -0400

    Update context for sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,16 +1,20 @@
 import { useContext } from "react";
 import "./SideBar.css";
-import avatar from "../../assets/WTWRAvatar.svg";
 import CurrentUserContext from "../../context/CurrentUserContext";
 
 function SideBar({ onLogout }) {
   const currentUser = useContext(CurrentUserContext);
+  const name = currentUser?.name;
+  const avatar = currentUser?.avatar;
+
+
+
 
   return (
     <div className="sidebar">
       <div className="sidebar__userinfo">
         <img className="sidebar__avatar" src={avatar} alt="Default" />
-        <p className="sidebar__username">Terrence Tegegne</p>
+        <p className="sidebar__username">{name}</p>
       </div>
       <button type="button" onClick={onLogout}>Log Out</button>
     </div>
