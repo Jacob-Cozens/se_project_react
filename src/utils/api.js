68a0f733619d73371a86a66ab6b4176b00e3994c
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-05-20 16:11:41 -0400

    Deploy frontend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.what2wear.crabdance.com"
+    : "http://localhost:3001";
 
 function processResponse(res) {
   if (res.ok) {
